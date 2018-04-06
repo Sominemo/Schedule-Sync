e779224ba64d0124a9e597abdfef7552057aec5d
Sominemo <dilongzhenya@gmail.com>
2018-04-07 00:02:46 +0300

    Added self-return and comments

--- method/users.get.php
+++ method/users.get.php
@@ -3,15 +3,25 @@ require_once('inc/api.php');
 
 new Auth();
 
+// Array for output
 $ra['response'] = [];
 
-if (isset($secure["query"])) $logins = explode(",", $secure["query"]);
-else $logins = $api_token_data["user_id"];
+// If client request anything
+if (isset($secure["query"])) {
+    // Handle multi-request
+    $logins = explode(",", $secure["query"]);
 
-foreach ($logins as $v) {
-    $u = new User($v, ['IGNORE_EXCEPTIONS' => true, 'GET_MODE' => 'login']);
-    $ra['response'][] = $u->get();
-}
+    // Handle each item
+    foreach ($logins as $v) {
+        // Get user     ...............................  !! Only by LOGIN
+        $u = new User($v, ['IGNORE_EXCEPTIONS' => true, 'GET_MODE' => 'login']);
+        $ra['response'][] = $u->get();
+    }
+
+    // Else return only current user
+} else $ra['response'][0] = $user->get();
+
+
 
 require_once('inc/end.php');
 
