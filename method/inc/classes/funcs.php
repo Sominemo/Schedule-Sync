16191a7f2e6345d7018f3e157f3f1dc6ecdd99f0
Sominemo <dilongzhenya@gmail.com>
2018-07-15 20:41:15 +0300

    String Check function

--- method/inc/classes/funcs.php
+++ method/inc/classes/funcs.php
@@ -64,4 +64,34 @@ class funcs
     public static function giveArray($a, $k) {
         if (isset($a[$k])) return $a[$k]; else return false;
     }
+
+    public static function strCheck($q, $o) {
+        if (!is_array($o) || !is_string($q)) return false;
+
+        $p = [];
+        $p['length'] = strlen($q);
+
+        // MIN Length
+        if (isset($o['min'])) {
+            if ($p['length'] < $o['min']) return false;
+        }
+
+        // MAX Length
+        if (isset($o['max'])) {
+            if ($p['length'] > $o['max']) return false;
+        }
+
+        // RegExp
+        if (isset($o['regex'])) {
+            if (!preg_match($o['regex'], $q)) return false;
+        }
+
+        // Symbols
+        if (isset($o['symbols'])) {
+            $slq = preg_quote($o['symbols']);
+            if (!preg_match('/([{$slq}]+)?/', $q)) return false;
+        }
+
+        return true;
+    }
 }
