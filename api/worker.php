2233714da887ff7bfc6ed301c75dd259bcb72c40
Sominemo <dilongzhenya@gmail.com>
2018-08-19 10:05:09 +0300

    Making METHOD_GET_VARIABLE_NAME more complex

--- api/worker.php
+++ api/worker.php
@@ -4,7 +4,7 @@ set_exception_handler(function() {
     echo json_encode(["error" => 100, "info" => "Something went wrong"]);
     die;
 });
-const __METHOD_GET_VARIABLE_NAME = "method_name_context";
+const __METHOD_GET_VARIABLE_NAME = "___method_|_name_|_context_|";
 require_once "inc/call_method.php";
 __MethodCaller\__Call::Init();
 require_once "inc/api.php";
