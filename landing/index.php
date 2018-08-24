5a0c91209d78e87a85fff4522ef287d06c6b86b7
Sominemo <dilongzhenya@gmail.com>
2018-08-24 22:48:37 +0300

    Updated landing

--- landing/index.php
+++ landing/index.php
@@ -8,21 +8,21 @@
     <link rel="stylesheet" href="res/css/main.css">
     <script src="res/js/word.js"></script>
 
-    <meta property="og:title" content="Coming this summer" />
+    <meta property="og:title" content="ProcSec Temply" />
     <meta property="og:image" content="res/img/preview.png" />
 
-    <meta property="twitter:title" content="Coming this summer" />
+    <meta property="twitter:title" content="ProcSec Templyr" />
     <meta property="twitter:image" content="res/img/preview.png" />
 
-    <meta property="mrc__share_title" content="Coming this summer" />
+    <meta property="mrc__share_title" content="ProcSec Temply" />
     <meta property="image" content="res/img/preview.png" />
 
-    <title>Coming soon</title>
+    <title>ProcSec Temply</title>
 </head>
 <body>
     <div class="content">
         <div class="wow-image"><img src="res/img/<?=rand(1,3); ?>.png" alt="Redesign Screenshot"></div>
-        <div class="poster"><div class="big">Coming Some Day</div><div class="small">Stay tuned</div></div>
+        <div class="poster"><div class="big"><img src="Temply_Full.png" style="max-width: 40vw;"></div><div class="small"></div></div>
     </div>
 </body>
 </html>
