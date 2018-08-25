8e7e95a0e21e37ddc541b4fedd213ee3ca34bc19
Sominemo <dilongzhenya@gmail.com>
2018-08-25 17:11:56 +0300

    Fix

--- api/ex/db_credentials_data.php
+++ api/ex/db_credentials_data.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ * DB auth data
+ *
+ * @package Temply-Account\Core\Static
+ * @author Sergey Dilong
+ * @license GPL-2.0
+ */
+
 // DB data
-define ('DBHOST', 'host');
-define ('DBNAME', 'name');
-define ('DBPASS', 'pass');
-define ('DBUSER', 'user');
+// TODO: Make DB data private
+/** @ignore */
+define ('DBHOST', 'sominemo.mysql.tools');
+/** @ignore */
+define ('DBNAME', 'sominemo_temply');
+/** @ignore */
+define ('DBPASS', 's1O4h1TPgr1D');
+/** @ignore */
+define ('DBUSER', 'sominemo_temply');
