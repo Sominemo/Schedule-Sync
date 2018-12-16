e8ce68f13632031eadb0968072e1ca394d5c214c
Sominemo <dilongzhenya@gmail.com>
2018-12-16 22:14:40 +0200

    Better error handling

--- api/worker.php
+++ api/worker.php
@@ -9,25 +9,38 @@
  * @license GPL-2.0
  */
 ini_set('display_errors', 0);
-set_exception_handler(function ($a) {
+$temp_shutdown_function = function () {
+    $a = error_get_last();
+    if(($a['type'] !== E_ERROR) && ($a['type'] !== E_USER_ERROR)) return;
     try {
-        $lf = "inc/priv/mcp.log";
-        $a = var_export($a, true);
+        $lf1 = __DIR__."/inc/priv/";
+        $lf2 = "mcp.log";
+        $lf = $lf1.$lf2;
+        $p = var_export($a, true);
         $l = "\n\n[" . time() . "] ";
-        if (file_put_contents($lf, [$l, $a], FILE_APPEND) === false) {
-            throw new Error();
+        if (file_put_contents($lf, $p.$l, FILE_APPEND) === false) {
+            throw new Exception();
         }
 
         echo ('{"error": 100, "info": "Core Initialization failed"}');
         die;
-    } catch (Error $e) {
-        echo ("<b>MCP failed to initialize core and write logs</b><br>");
-        var_export($a);
+    } catch (Exception $e) {
+        $d = "";
+        if (!is_dir($lf1) || !is_writable($lf1)) {
+            $d .= ": Directory doesn't exist or unwritable (".$lf.")";
+        } 
+        if (is_file($lf) && !is_writable($lf)) {
+            $d .= ": File exists, but unwritable";
+        }
+        echo ("<b>MCP failed to initialize core and write logs</b>".$d."<br>");
         die;
     } finally {
         die('<b><font color="red">The system is non-working</font></b>');
     }
-});
+};
+set_exception_handler($temp_shutdown_function);
+register_shutdown_function($temp_shutdown_function);
+unset($temp_shutdown_function);
 
 // Record method name
 /** Name of GET-variable, which contains User-requested name of method, given by Apache */
